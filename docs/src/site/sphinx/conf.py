628c1a002e99264f0b6195ad6c24fe9f43916647
Jorge L. Williams <jorge.williams@rackspace.com>
2017-09-26 23:13:58 -0500

    Update title.

--- docs/src/site/sphinx/conf.py
+++ docs/src/site/sphinx/conf.py
@@ -57,7 +57,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Attribute Mapping'
+project = u'Attribute Mapping Policy Reference'
 copyright = u'2017, Rackspace'
 author = u'Rackspace'
 
