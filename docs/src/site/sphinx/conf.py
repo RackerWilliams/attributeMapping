7c5d2ce2713617de6510913858a369666999d926
Jorge L. Williams <jorge.williams@rackspace.com>
2017-10-02 15:20:50 -0500

    Let's not show source link.

--- docs/src/site/sphinx/conf.py
+++ docs/src/site/sphinx/conf.py
@@ -135,6 +135,7 @@ html_use_smartypants = True
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_show_sourcelink = False
 
 # -- Options for HTMLHelp output ------------------------------------------
 
